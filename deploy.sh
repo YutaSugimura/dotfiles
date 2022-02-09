18816f2bab9093477bec947e7404ef4f32430c43
YutaSugimura <nextc3m@gmail.com>
2022-02-09 14:17:19 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,4 @@ dotfiles=(.zshrc .tmux.conf)
 # .zshrc と .tmux.conf という設定ファイルのシンボリックリンクを
 # ホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
-        ln -svf $file ~/
-done
-
+        ln -svf ~/dotfiles/${file} ~/${file}
